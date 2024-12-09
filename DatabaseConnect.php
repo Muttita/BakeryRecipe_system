7f999b0ffe31124988793180afba71ca0c1ef44a
Muttita Chandawong <159625061+Muttita@users.noreply.github.com>
2024-12-09 23:34:22 +0700

    Add files via upload

---
+++ DatabaseConnect.php
@@ -0,0 +1,8 @@
+<?php 
+  // Connection to database
+  $servername = "10.199.120.92";
+  $username = "Hello";
+  $passwordmysql = "Hello12345678.";
+  $dbn = "databaseproject";
+  $conn = new mysqli($servername, $username, $passwordmysql, $dbn);
+?>
